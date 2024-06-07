e1ac4d6c7593ef135615c094e64b85a573b37c74
K0unty <kountiapi@pm.me>
2024-06-08 00:31:27 +0530

    FixPrettierDefaultFormatter

--- L/S1/UTS1/first-project/first-project/js-only.js
+++ L/S1/UTS1/first-project/first-project/js-only.js
@@ -6,10 +6,8 @@ function add(num1, num2) {
   return num1 + num2;
 }
 
-
-
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
   console.log(add(input1.value, input2.value));
 });
 
-console.log('sds    ')
+console.log("sds    ");
