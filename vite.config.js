b2e2d826ec9f048a21eeb1d9b7db5fb16b5b6876
K0unty <kountiapi@pm.me>
2024-06-09 07:15:59 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from "vite";
-import glob from "glob";
 
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: async () => {
-        const files = await glob("src/L/**/*.index.html", {
-          cwd: process.cwd(),
-          absolute: true,
-        });
-        return files;
-      },
+      input: [
+        "/index.html",
+        "/L/**/index.html", // include all index.html files in subdir and its subdirectories
+      ],
     },
   },
 });
