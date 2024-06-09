5ff3cf7929e37971a765b6fb314822f0fd42e464
K0unty <kountiapi@pm.me>
2024-06-10 02:41:19 +0530

    17

---
+++ src/L/S2/app.ts
@@ -0,0 +1,24 @@
+/* 
+17: Objects 
+*/
+
+console.log(
+  `
+  %cSection 2 - 17 - Objects
+  ===========
+  `,
+  "color:yellow;font-size:1.2rem"
+);
+
+// Work here
+
+// basic object
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
+  name: "Ina",
+  age: 18,
+};
+console.log(person.name);
