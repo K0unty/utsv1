a0df7fe08320af9bc3710a3f3e7612f845b018ef
K0unty <kountiapi@pm.me>
2024-06-09 07:22:09 +0530

    again

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ export default defineConfig({
     rollupOptions: {
       input: [
         "/index.html",
-        "L/S1/UTS1/adjusted-project/adjusted-project/index.html",
-        "L/S1/UTS1/first-project/first-project/index.html",
+        "L/S1/UTS1/adjusted-project/adjusted-project/**/*",
+        "L/S1/UTS1/first-project/first-project/**/*",
       ],
     },
   },
