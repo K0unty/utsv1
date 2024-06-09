148cf04d42c88f14b906e28d2695c13a22adedd3
K0unty <kountiapi@pm.me>
2024-06-09 06:40:23 +0530

    addnewVIte

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    // ... other build options
     rollupOptions: {
-      input: "./index.html", // Adjust if your entry point is different
-      output: {
-        include: ["**/*.html", "src/L/**"], // Include HTML and subdirectory
+      input: {
+        main: "/index.html",
+        "custom-dir": "/src/custom-dir/index.html", // Add this line
       },
     },
   },
