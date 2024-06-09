649efe5bbd4f8df201ed4f5ad8f7f9f236fecd61
K0unty <kountiapi@pm.me>
2024-06-09 07:11:32 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-mport { defineConfig, glob } from 'vite';
-import path from 'path';
+import { defineConfig, glob } from "vite";
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: async () => {
-        const files = await glob('src/L/**/*.index.html', {
+        const files = await glob("src/L/**/*.index.html", {
           cwd: process.cwd(),
           absolute: true,
         });
