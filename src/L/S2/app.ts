99c2314853da398c0394f3430701ba4a3eb6c8a4
K0unty <kountiapi@pm.me>
2024-06-10 02:04:09 +0530

    add

--- src/L/S2/app.ts
+++ src/L/S2/app.ts
@@ -9,3 +9,17 @@ console.log(
 `,
   "color:yellow;font-size:1.2rem"
 );
+
+const addNumber = function add(n1: number, n2: number) {
+  // This is the JS way
+  // if (typeof n1 === "number" && typeof n2 === "number") {
+  return n1 + n2;
+  // } else {
+  console.log("Fuck o NUm");
+  // }
+};
+const number1 = 5;
+const number2 = 2.8;
+
+const result = addNumber(number1, number2);
+console.log(result);
