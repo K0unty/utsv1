d24db2a570ee49470222b572db01407f628766e1
K0unty <kountiapi@pm.me>
2024-06-10 06:33:56 +0530

    23 UnionTypes

--- src/L/S2/23.ts
+++ src/L/S2/23.ts
@@ -9,3 +9,26 @@ console.log(
       `,
   "color:yellow;font-size:1.2rem"
 );
+
+// Work
+
+const combine = function combine(
+  input1: number | string,
+  input2: number | string
+) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+};
+
+const combAges = combine(30, 26);
+console.log(combAges);
+
+const combNam = combine("Oin", "jin");
+console.log(combNam);
+
+// console.log(combine(21, 22));
