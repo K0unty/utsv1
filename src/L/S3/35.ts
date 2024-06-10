d6c922a89f47bcd5ff792341758ac7692283c058
K0unty <kountiapi@pm.me>
2024-06-11 02:04:38 +0530

    add 36

---
+++ src/L/S3/35.ts
@@ -0,0 +1,21 @@
+/* 
+35 - Using Watch mode 
+- This lesson was about using watch mode 
+
+> ts <tsfile>.ts -w 
+
+*/
+
+console.log(
+  `
+%cS3-35 - Using Watch Mode
+===========
+            `,
+  "color:yellow;font-size:1.2rem"
+);
+
+// ==== Work zone  ===
+
+const userName = "inass ";
+
+console.log(userName);
---
+++ src/L/S3/36.ts
@@ -0,0 +1,18 @@
+/* 
+36 - Compilng the entire project
+*/
+
+console.log(
+  `
+%cS3-36 - Compiling Entire Project / Multiple Files
+===========
+            `,
+  "color:yellow;font-size:1.2rem"
+);
+
+// ==== Work zone  ===
+
+const userName1 = "inass ";
+
+console.log("Sending....");
+console.log(userName1);
