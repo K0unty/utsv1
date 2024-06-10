167c53f4cb5862afef1a181180f5bf93b9c8ef89
K0unty <kountiapi@pm.me>
2024-06-10 09:20:29 +0530

    add markdown

--- src/L/S2/27.ts
+++ src/L/S2/27.ts
@@ -1,3 +1,14 @@
 /* 
 27: Function Return Types and Void
 */
+
+const add1 = function (n1: number, n2: number): number {
+  return n1 + n2;
+};
+
+const printRes = function (num: number): void {
+  console.log("Result : " + num);
+  // Its called void because it doesnt have a return statement
+};
+
+printRes(add1(5, 12));
