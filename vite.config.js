f2bf3ce928cb7f8d9b219a6b45cac3a821fba077
K0unty <kountiapi@pm.me>
2024-06-10 06:46:39 +0530

    fixVite

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ module.exports = {
         main: "/index.html",
         sub1: "src/L/S1/UTS1/first-project/first-project/index.html",
         sub2: "src/L/S1/UTS1/adjusted-project/adjusted-project/index.html",
-        sub2: "src/L/S2/index.html",
+        sub3: "src/L/S2/index.html",
       },
     },
   },
