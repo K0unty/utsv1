2e33702fcdabc415e2932804edf61628cf88a34b
K0unty <kountiapi@pm.me>
2024-06-10 06:55:50 +0530

    addVercelandVite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-// vite.config.js
-export default {
-  //...
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: "/index.html",
-        sub1: "src/L/S1/UTS1/first-project/first-project/index.html",
-        sub2: "src/L/S1/UTS1/adjusted-project/adjusted-project/index.html",
-        sub3: "src/L/S2/index.html",
+        main: path.resolve(__dirname, "index.html"),
+        subpage1: path.resolve(__dirname, "src/L/S2/index.html"),
+        // Add more entries as needed
       },
     },
   },
-};
+});
