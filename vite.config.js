280a7b0ef13d0ef0d3b2d6e1e3b08b95441b25d3
K0unty <kountiapi@pm.me>
2024-06-10 06:44:47 +0530

    addVite

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  //...
+  build: {
+    rollupOptions: {
+      input: {
+        main: "/index.html",
+        sub1: "src/L/S1/UTS1/first-project/first-project/index.html",
+        sub2: "src/L/S1/UTS1/adjusted-project/adjusted-project/index.html",
+        sub2: "src/L/S2/index.html",
+      },
+    },
+  },
+};
