79fe76c536f8abf8635e86ecb4a684e77b35f19d
K0unty <kountiapi@pm.me>
2024-06-11 02:47:05 +0530

    add39

---
+++ src/L/S3/38.ts
@@ -0,0 +1,16 @@
+/* 
+38 - Setting a compilation tareget
+
+
+```
+*/
+console.log(
+  `
+%cS3-38=Setting a compilation target
+===========
+            `,
+  "color:yellow;font-size:1.2rem"
+);
+
+// Work Zone
+console.log("Sending.....");
---
+++ src/L/S3/39.ts
@@ -0,0 +1,16 @@
+/* 
+39 - Understanding Typescript Core Libs
+
+
+```
+*/
+console.log(
+  `
+%cS3-39 - Understanding Typescript Core Libs
+===========
+            `,
+  "color:yellow;font-size:1.2rem"
+);
+
+// Work Zone
+console.log("Sending.....");
