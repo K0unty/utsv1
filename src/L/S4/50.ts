b5af188e0e25bae1056f5861cc14df68b591094a
K0unty <kountiapi@pm.me>
2024-06-11 06:22:16 +0530

    s4

---
+++ src/L/S4/50.ts
@@ -0,0 +1,17 @@
+/* 
+50 - 'let' and 'const' variables 
+*/
+console.log(
+  `
+  %cS4-50 - 'let' and 'const' variables 
+  ===========
+              `,
+  "color:yellow;font-size:1.2rem"
+);
+
+// Work Zone
+
+const userNameS4 = "Jim";
+let age = 20;
+
+age = 19;
