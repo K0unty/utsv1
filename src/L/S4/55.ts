69501e854d724fb2ebe4e84533d83f0a5a94e6ff
K0unty <kountiapi@pm.me>
2024-06-11 07:32:46 +0530

    55

---
+++ src/L/S4/55.ts
@@ -0,0 +1,11 @@
+/* 
+55 - Array and Object Destructuring 
+*/
+
+console.log(
+  `
+%cS 4 - 55 - Array and Object Destructuring 
+===========
+              `,
+  "color:yellow;font-size:1.2rem"
+);
