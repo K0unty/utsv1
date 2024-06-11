9e3b3bdc76cf1ddd09d66f96312eb3d2151a9261
K0unty <kountiapi@pm.me>
2024-06-11 06:32:14 +0530

    50

--- src/L/S4/50.ts
+++ src/L/S4/50.ts
@@ -12,6 +12,23 @@ console.log(
 // Work Zone
 
 const userNameS4 = "Jim";
+// userNameS4 = "jin";
 let age = 20;
 
 age = 19;
+
+// var  - not to use this anymore instead use let
+
+// r4egading scopes ehre
+const add4 = function (a: number, b: number) {
+  let result;
+  result = a + b;
+  return result;
+};
+
+// console.log(result);
+
+if (age >= 20) {
+  let isOld = true;
+  console.log(isOld);
+}
