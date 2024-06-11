ab5e4dde11d9435f2ebfe8adfe80a542ff4d110a
K0unty <kountiapi@pm.me>
2024-06-11 08:57:00 +0530

    62

--- src/L/S5/62.ts
+++ src/L/S5/62.ts
@@ -1,10 +1,10 @@
 /* 
-60: Creating your first class 
+62: Constructor Functions and The "this keyword"
 */
 
 console.log(
   `
-  %cS5 - 62: Creating a class  
+  %cS5 - 62: Constructor Functions and This Keyword
   ===========
                 `,
   "color:yellow;font-size:1.2rem"
@@ -13,14 +13,26 @@ console.log(
 // Work Zone
 
 // Creating a class - Making muiltiple departments in a company
-class Department {
+class Department2 {
   name: string = "DEFAULT";
 
+  // Constructor Function
   constructor(n: string) {
     this.name = n;
+  }
+
+  // Methods
+  describe(this: Department2) {
+    console.log(`Deparment⚙️: ` + this.name);
   }
 }
 
 // Definign a new department with the class above
-const acc1 = new Department("Acc");
-console.log(acc1);
+const acc2 = new Department2("Acc");
+// console.log(acc2);
+
+// Calling the described method from the depeartment class
+acc2.describe();
+
+const acc2Copy = { name: "Dummy", describe: acc2.describe };
+acc2Copy.describe();
