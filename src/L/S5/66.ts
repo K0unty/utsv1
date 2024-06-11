c2ea70a73e1020665802ae2bdb92106fd4387303
K0unty <kountiapi@pm.me>
2024-06-11 10:03:57 +0530

    66

---
+++ src/L/S5/66.ts
@@ -0,0 +1,56 @@
+/* 
+66: Inheritance
+*/
+
+console.log(
+  `
+  %cS5 - 66: Inheritance
+  ===========
+                `,
+  "color:yellow;font-size:1.2rem"
+);
+
+// Work Zone
+
+// Creating a class - Making muiltiple departments in a company
+class Department6 {
+  // private readonly id: string;
+  // private name: string = "DEFAULT";
+  private empl: string[] = [];
+
+  // Constructor Function - adding the readonly keyword at this construtor
+  constructor(private readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
+  }
+
+  // Methods
+  describe(this: Department6) {
+    console.log(`Dep [${this.id} - ${this.name}]`);
+  }
+
+  // Addign employee Method
+  addEmpl(empl: string) {
+    // this.id = "d2";
+    this.empl.push(empl);
+  }
+
+  // Printing the added emplyee
+  printEmInfo() {
+    console.log(this.empl.length);
+    console.log(this.empl);
+  }
+}
+
+// Definign a new department with the class above
+const acc6 = new Department6("d1", "Acc");
+
+acc6.addEmpl("bin");
+acc6.addEmpl("Nal");
+
+// Adding employee directrly since that variable can be called - Alternative method shouldnt be there
+// acc6.empl[2] = "hahah";
+acc6.name = "Fuku";
+
+acc6.describe();
+acc6.printEmInfo();
