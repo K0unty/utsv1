8660ddc4de2fc58ed20587eae7d64240a0e5d890
K0unty <kountiapi@pm.me>
2024-06-11 07:43:37 +0530

    55

--- src/L/S4/55.ts
+++ src/L/S4/55.ts
@@ -9,3 +9,47 @@ console.log(
               `,
   "color:yellow;font-size:1.2rem"
 );
+
+// Work
+
+// add funcion - done with array method reduce
+const add55 = (...num: number[]) => {
+  return num.reduce((cuResult, curValue) => {
+    return cuResult + curValue;
+  }, 0);
+};
+
+const addedNumbers55 = add55(5, 10, 2.1, 5, 9, 9, 9);
+console.log(addedNumbers55);
+
+//
+
+const hob2 = ["spo", "cuk", "jan", "fan"];
+const hob54 = hob2[0];
+const hob542 = hob2[1];
+console.log(hob54, hob542);
+
+// Destrututin
+
+const [ho1, ho2, ...remainingHobbies] = hob2;
+console.log(`%cDestructured Array`, "font-size:18px");
+console.log(ho1);
+console.log(ho2);
+console.log(remainingHobbies);
+
+// Object Destructuring
+
+let per55 = {
+  name55: "fang",
+  age55: "99",
+};
+console.log(
+  `
+%cDestructured Object
+`,
+  "font-size:18px"
+);
+
+// here also storing the key in another key
+const { name55: bondy, age55 } = per55;
+console.log(bondy, age55);
