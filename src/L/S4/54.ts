8b7404f3444e83685f3cc37145c9ee10260572a6
K0unty <kountiapi@pm.me>
2024-06-11 07:26:30 +0530

    54.ts

---
+++ src/L/S4/54.ts
@@ -0,0 +1,42 @@
+/* 
+54 - Rest parameters
+*/
+console.log(
+  `
+%cS 4 - 54 - Rest parameters
+===========
+              `,
+  "color:yellow;font-size:1.2rem"
+);
+
+const hob1 = ["spo", "wom"];
+const actHob1 = ["hik", ...hob1];
+
+// using spread operator to push the array elements
+// ?actHob.push(...hob);
+console.log(actHob1);
+
+// Exists on object
+
+const per11 = {
+  name: "mina",
+  age: "20",
+};
+
+// This is just a pointer
+const copyPer11 = per11;
+console.log(per11);
+
+// making copy of object
+const copPer21 = { ...per11 };
+console.log(copPer21);
+
+// add funcion - done with array method reduce
+const add54 = (...num: number[]) => {
+  return num.reduce((cuResult, curValue) => {
+    return cuResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add54(5, 10, 2.1, 5, 9, 9, 9);
+console.log(addedNumbers);
