9459ddc4d864c1a0bee1d52467632b488d1cbd57
K0unty <kountiapi@pm.me>
2024-06-13 01:19:59 +0530

    91

--- src/L/S6/91.ts
+++ src/L/S6/91.ts
@@ -1,49 +1,49 @@
 /* 
-90: Optional Chaining 
+91: Optional Chaining 
 */
 console.log(
   `
-      %cS5 - 90: Optional Chaining
+      %cS5 - 91: Optional Chaining
       =============================================`,
-  "color:white;font-size:1.2rem;background-color:#19071e"
+  "color:white;font-size:1.2rem;background-color:#19171e"
 );
 
 // Work here
 
-type Admin90 = {
+type Admin91 = {
   name: string;
   priv: string[];
 };
 
-type Empl90 = {
+type Empl91 = {
   name: string;
   startDate: Date;
 };
 
 //Create a new type = Elevated Employee
-type ElevEmpl90 = Admin90 & Empl90;
+type ElevEmpl91 = Admin91 & Empl91;
 
-const e190: ElevEmpl90 = {
+const e191: ElevEmpl91 = {
   name: "Fon",
   priv: ["make-server"],
   startDate: new Date(),
 };
 
 // Print this types
-console.log(e190);
+console.log(e191);
 
-type Combina90 = string | number;
-type Numeric90 = number | boolean;
+type Combina91 = string | number;
+type Numeric91 = number | boolean;
 
-type Universal90 = Combina & Numeric;
+type Universal91 = Combina & Numeric;
 
-// 90 - Uusing this function
+// 91 - Uusing this function
 
-function add90(a: number, b: number): number;
-function add90(a: string, b: string): string;
-function add90(a: string, b: number): string;
-function add90(a: number, b: number): string;
-function add90(a: Combina90, b: Combina90) {
+function add91(a: number, b: number): number;
+function add91(a: string, b: string): string;
+function add91(a: string, b: number): string;
+function add91(a: number, b: number): string;
+function add91(a: Combina91, b: Combina91) {
   // TypeGuard
   if (typeof a === "string" || typeof b === "string") {
     return a.toString() + b.toString();
@@ -51,13 +51,13 @@ function add90(a: Combina90, b: Combina90) {
   return a + b;
 }
 
-const result90 = add90("Ina", 69);
-console.log(result90);
-result90.split(" ");
+const result91 = add91("Ina", 69);
+console.log(result91);
+result91.split(" ");
 
-//90: optional chianing OC
+//91: optional chianing OC
 
-const fetchUserData90 = {
+const fetchUserData91 = {
   id: "ul",
   name: "ina",
   // job: {
@@ -66,13 +66,13 @@ const fetchUserData90 = {
   // },
 };
 
-console.log(fetchUserData90);
-// console.log(fetchUserData90?.job?.title);
+console.log(fetchUserData91);
+// console.log(fetchUserData91?.job?.title);
 
-// console.log(add90(2, 1));
+// console.log(add91(2, 1));
 
-// type UnkEmpl90 = Empl90 | Admin90;
-// function prEmpInf(emp: UnkEmpl90) {
+// type UnkEmpl91 = Empl91 | Admin91;
+// function prEmpInf(emp: UnkEmpl91) {
 //   console.log("Name: " + emp.name);
 
 //   if ("priv" in emp) {
@@ -86,11 +86,11 @@ console.log(fetchUserData90);
 // // Printing the employee Information.
 // console.log(
 //   `
-// %ce190 Info
+// %ce191 Info
 // `,
 //   "font-style:oblique"
 // );
-// prEmpInf(e190);
+// prEmpInf(e191);
 
 // // Making a new employee
 // console.log(
@@ -106,12 +106,12 @@ console.log(fetchUserData90);
 
 // // Testing union of classes here with the type guards
 
-// class car90 {
+// class car91 {
 //   drive() {
 //     console.log("driving");
 //   }
 // }
-// class truck90 {
+// class truck91 {
 //   drive() {
 //     console.log("Driving Truck...");
 //   }
@@ -121,35 +121,35 @@ console.log(fetchUserData90);
 //   }
 // }
 
-// type Vehicle90 = car90 | truck90;
+// type Vehicle91 = car91 | truck91;
 
-// const v190 = new car90();
-// const v290 = new truck90();
+// const v191 = new car91();
+// const v291 = new truck91();
 
-// function useVehicle90(vehicle: Vehicle90) {
+// function useVehicle91(vehicle: Vehicle91) {
 //   vehicle.drive();
-//   if (vehicle instanceof truck90) {
+//   if (vehicle instanceof truck91) {
 //     vehicle.loadCargo(1000);
 //   }
 // }
 
-// useVehicle90(v190);
-// useVehicle90(v290);
+// useVehicle91(v191);
+// useVehicle91(v291);
 
-// // 90 - Work here foor discriminated unions
-// interface Bird90 {
+// // 91 - Work here foor discriminated unions
+// interface Bird91 {
 //   type: "bird"; // Literal type assigment
 //   flyingSpeed: number;
 // }
 
-// interface Horse90 {
+// interface Horse91 {
 //   type: "horse";
 //   runningSpeed: number;
 // }
 
 // // union type
-// type Animal90 = Bird90 | Horse90;
-// const moveAnimal90 = function (animal: Animal90) {
+// type Animal91 = Bird91 | Horse91;
+// const moveAnimal91 = function (animal: Animal91) {
 //   let speed;
 //   switch (animal.type) {
 //     case "bird":
@@ -161,34 +161,34 @@ console.log(fetchUserData90);
 //   console.log(animal.type + " Moving at speed " + speed);
 // };
 
-// moveAnimal90({
+// moveAnimal91({
 //   type: "bird",
 //   flyingSpeed: 10,
 // });
 
-// //90 Type Casting
-// // const userInputElement90 = <HTMLInputElement>(
+// //91 Type Casting
+// // const userInputElement91 = <HTMLInputElement>(
 // //   document.getElementById("user-input")!
 // // );
-// // const userInputElement90 = document.getElementById(
+// // const userInputElement91 = document.getElementById(
 // //   "user-input"
 // // )! as HTMLInputElement;
 
-// // userInputElement90.value = "FuckOff";
+// // userInputElement91.value = "FuckOff";
 
 // // // if ! is not used you have to do a type check
-// // if (userInputElement90) {
-// //   (userInputElement90 as HTMLInputElement).value = "Fuckoff";
+// // if (userInputElement91) {
+// //   (userInputElement91 as HTMLInputElement).value = "Fuckoff";
 // // }
 
-// // 90 Work there -
+// // 91 Work there -
 
-// interface ErrorContainer90 {
+// interface ErrorContainer91 {
 //   // Error object identifier - { email: not valid, username: 'Start with char'}
 //   [prop: string]: string; //every prop will be a string with value string
 // }
 
-// const errorBag90: ErrorContainer90 = {
+// const errorBag91: ErrorContainer91 = {
 //   email: "Wrong Email",
 //   uname: "Must be  Capital",
 // };
