5ddb3417d6f010b3602d08d6ecc2233017dd8717
K0unty <kountiapi@pm.me>
2024-06-12 09:31:25 +0530

    add bg image

---
+++ src/L/S6/84.ts
@@ -0,0 +1,11 @@
+/* 
+84: Intersection Types
+*/
+console.log(
+  `
+      %cS5 - 80: Compiling Interfaces to Javascript
+      =============================================`,
+  "color:white;font-size:1.2rem;background-color:#19071e"
+);
+
+// Work here
