9f99493ef07081dc3635f2bdd94668395023f5e1
K0unty <kountiapi@pm.me>
2024-06-12 10:22:43 +0530

    84

--- src/L/S6/84.ts
+++ src/L/S6/84.ts
@@ -9,3 +9,30 @@ console.log(
 );
 
 // Work here
+
+type Admin = {
+  name: string;
+  priv: string[];
+};
+
+type Empl = {
+  name: string;
+  startDate: Date;
+};
+
+//Create a new type = Elevated Employee
+type ElevEmpl = Admin & Empl;
+
+const e1: ElevEmpl = {
+  name: "Fon",
+  priv: ["make-server"],
+  startDate: new Date(),
+};
+
+// Print this types
+console.log(e1);
+
+type Combina = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combina & Numeric;
