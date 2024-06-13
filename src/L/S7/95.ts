36a440ab2d60f4047b824d2b4d641476ab7de83b
K0unty <kountiapi@pm.me>
2024-06-13 07:00:34 +0530

    add colo.ts

--- src/L/S7/95.ts
+++ src/L/S7/95.ts
@@ -1,10 +1,34 @@
 /* 
 95: Built In generics
 */
+
+// Import Zone
+import { logToConsole } from "../../colo";
+// ---
+
 console.log(
   `
 %cS5 - 91: Nullish Coalescing (nC) `,
   "background: linear-gradient(to right, rgba(38, 2, 33, 0.906) 55%, rgba(22, 0, 98, 1) 99%); color:#FFD933; font-size: 24px; padding: 10px;"
 );
 
+// // Define a function to append messages to the console output div
+// function logToConsole(message: string, style: string) {
+//   console.log(`%c${message}`, style);
+//   const consoleOutputDiv = document.getElementById("console-output");
+//   if (consoleOutputDiv) {
+//     const messageElement = document.createElement("div");
+//     messageElement.style.cssText = style;
+//     messageElement.textContent = message;
+//     consoleOutputDiv.appendChild(messageElement);
+//   }
+// }
+
 // Actual work her
+
+logToConsole("bootyDancer", "color:green");
+logToConsole("bootyDancer", "color:blue");
+logToConsole("bootyDancer", "color:blue");
+logToConsole("bootyDancer", "color:blue");
+logToConsole("bootyDancer", "color:blue");
+logToConsole("bootyDancer", "color:blue");
