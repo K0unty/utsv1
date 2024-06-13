3dcfef82050195af7423f7d9f6fb3874d3523822
K0unty <kountiapi@pm.me>
2024-06-13 05:58:38 +0530

    ConsoleLogFix

---
+++ src/L/S7/95.ts
@@ -0,0 +1,8 @@
+/* 
+95: Built In generics
+*/
+console.log(
+  `
+%cS5 - 91: Nullish Coalescing (nC) `,
+  "background: linear-gradient(to right, rgba(38, 2, 33, 0.906) 55%, rgba(22, 0, 98, 1) 99%); color: red; font-size: 24px; padding: 10px;"
+);
