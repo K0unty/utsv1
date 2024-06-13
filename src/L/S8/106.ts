5f9c2e565b21fd8c4705dfa198922be88f7d7410
K0unty <kountiapi@pm.me>
2024-06-14 00:35:00 +0530

    106

---
+++ src/L/S8/106.ts
@@ -0,0 +1,24 @@
+/* 
+106: A first Class Decorator
+*/
+
+// helper function
+
+const blackConsoleText = function (text: string) {
+  console.log(
+    `
+  %c${text}
+  `,
+    "background:black"
+  );
+};
+
+// ---
+
+console.log(
+  `
+  %cS8 - 106: A first Class Decorator `,
+  "background: linear-gradient(to right, rgba(38, 2, 33, 0.906) 47%, rgba(22, 0, 103, 1) 89%); color:#FFD933; font-size: 24px; padding: 10px;font-style:italic"
+);
+
+// Actual work her
