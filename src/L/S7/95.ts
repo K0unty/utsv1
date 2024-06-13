d3a1675f87df3ce244b348e7ea00d1822928821e
K0unty <kountiapi@pm.me>
2024-06-13 06:33:09 +0530

    95NiceConsoleBanner

--- src/L/S7/95.ts
+++ src/L/S7/95.ts
@@ -4,5 +4,7 @@
 console.log(
   `
 %cS5 - 91: Nullish Coalescing (nC) `,
-  "background: linear-gradient(to right, rgba(38, 2, 33, 0.906) 55%, rgba(22, 0, 98, 1) 99%); color: red; font-size: 24px; padding: 10px;"
+  "background: linear-gradient(to right, rgba(38, 2, 33, 0.906) 55%, rgba(22, 0, 98, 1) 99%); color:#FFD933; font-size: 24px; padding: 10px;"
 );
+
+// Actual work her
