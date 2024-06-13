fe059b5d02a37cdb06a413eb9b18515b7549adb6
K0unty <kountiapi@pm.me>
2024-06-13 10:57:20 +0530

    fix

--- src/L/S7/100.ts
+++ src/L/S7/100.ts
@@ -1,24 +1,25 @@
 /* 
-99: The 'keyof'
+100: Generic Classes
 */
 
 // helper function
 
-const blackConsoleText = function (text: string) {
-  console.log(
-    `
-%c${text}
-`,
-    "background:black"
-  );
-};
+// Since this has already been declaredred in 99.ts no need to call it back and available on all files now
+// const blackConsoleText = function (text: string) {
+//   console.log(
+//     `
+// %c${text}
+// `,
+//     "background:black"
+//   );
+// };
 
 // ---
 
 console.log(
   `
-%cS5 - 99: The 'keyof' `,
-  "background: linear-gradient(to right, rgba(38, 2, 33, 0.906) 55%, rgba(22, 0, 99, 1) 89%); color:#FFD933; font-size: 24px; padding: 10px;"
+%cS5 - 100: Generic Classes `,
+  "background: linear-gradient(to right, rgba(38, 2, 33, 0.906) 55%, rgba(22, 0, 100, 1) 89%); color:#FFD933; font-size: 24px; padding: 10px;"
 );
 
 // Actual work her
@@ -26,33 +27,33 @@ console.log(
 // Default type which also a generic
 
 // // Array is a generic
-// const names99: Array<string> = ["ina", "jan"];
-// // names99[0].split(" ");
+// const names100: Array<string> = ["ina", "jan"];
+// // names100[0].split(" ");
 
-// const promise99: Promise<number> = new Promise((resolve, reject) => {
+// const promise100: Promise<number> = new Promise((resolve, reject) => {
 //   setTimeout(() => {
 //     resolve(10);
 //     reject(Error);
 //   });
 // });
 
-// promise99.then((data) => {
+// promise100.then((data) => {
 //   // data.split("");
 // });
 
-//99 - Work here
+//100 - Work here
 
 // Function that merges two objects and returns an object
 /* 
 This function is different from the lesson , due to some issues the new syntax is 
 type parameters T and U using the extends keyword. The constraint extends object ensures that T and U must be objects. This is because the Object.assign method only works with objects, and we want to ensure that the function is used correctly.
 */
-function merge99<T extends object, U extends object>(objA: T, objB: U) {
+function merge100<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
-const meOb99 = merge99({ name: "Nip", hobbies: ["cooking"] }, { age: 30 });
-console.log(meOb99);
+const meOb100 = merge100({ name: "Nip", hobbies: ["cooking"] }, { age: 30 });
+console.log(meOb100);
 
 console.log(
   `%c
@@ -60,16 +61,16 @@ Printing Parts of object
 `,
   "background:black"
 );
-console.log(meOb99.age);
+console.log(meOb100.age);
 
-// 99: Another generic function
+// 100: Another generic function
 // Count and print function
 
 interface Lengthy {
   length: number;
 }
 
-function countAndDesc99<T extends Lengthy>(element: T): [T, string] {
+function countAndDesc100<T extends Lengthy>(element: T): [T, string] {
   let descriptionText = "Got No Val";
   if (element.length === 1) {
     descriptionText = "Got 1 Element";
@@ -84,13 +85,13 @@ Printing countAnDesc09 Function
 `,
   "background:black"
 );
-console.log(meOb99.age);
+console.log(meOb100.age);
 
-console.log(countAndDesc99("Whatuo"));
-console.log(countAndDesc99(["Sports", "Cooking"]));
+console.log(countAndDesc100("Whatuo"));
+console.log(countAndDesc100(["Sports", "Cooking"]));
 
-// 99 Keyof Function
-function extractAndConvert99<T extends object, U extends keyof T>(
+// 100 Keyof Function
+function extractAndConvert100<T extends object, U extends keyof T>(
   obj: T,
   key: U
 ) {
@@ -98,4 +99,4 @@ function extractAndConvert99<T extends object, U extends keyof T>(
 }
 
 blackConsoleText("Printing extractandConvert");
-extractAndConvert99({ name: "boing" }, "name");
+extractAndConvert100({ name: "boing" }, "name");
