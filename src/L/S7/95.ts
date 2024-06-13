2ddd1fe1d78202771b4afbcba8966e650009ed0d
K0unty <kountiapi@pm.me>
2024-06-13 07:07:28 +0530

    fix function logtoconsole

--- src/L/S7/95.ts
+++ src/L/S7/95.ts
@@ -3,7 +3,7 @@
 */
 
 // Import Zone
-import { logToConsole } from "../../colo";
+
 // ---
 
 console.log(
@@ -12,17 +12,17 @@ console.log(
   "background: linear-gradient(to right, rgba(38, 2, 33, 0.906) 55%, rgba(22, 0, 98, 1) 99%); color:#FFD933; font-size: 24px; padding: 10px;"
 );
 
-// // Define a function to append messages to the console output div
-// function logToConsole(message: string, style: string) {
-//   console.log(`%c${message}`, style);
-//   const consoleOutputDiv = document.getElementById("console-output");
-//   if (consoleOutputDiv) {
-//     const messageElement = document.createElement("div");
-//     messageElement.style.cssText = style;
-//     messageElement.textContent = message;
-//     consoleOutputDiv.appendChild(messageElement);
-//   }
-// }
+// Define a function to append messages to the console output div
+function logToConsole(message: string, style: string) {
+  console.log(`%c${message}`, style);
+  const consoleOutputDiv = document.getElementById("console-output");
+  if (consoleOutputDiv) {
+    const messageElement = document.createElement("div");
+    messageElement.style.cssText = style;
+    messageElement.textContent = message;
+    consoleOutputDiv.appendChild(messageElement);
+  }
+}
 
 // Actual work her
 
