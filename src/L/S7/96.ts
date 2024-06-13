8b0371f3819aad0017e4bf038e50077ac3c93b4f
K0unty <kountiapi@pm.me>
2024-06-13 07:39:18 +0530

    96

--- src/L/S7/96.ts
+++ src/L/S7/96.ts
@@ -1,5 +1,5 @@
 /* 
-95: Built In generics
+96: Bulding a Generic Function
 */
 
 // Import Zone
@@ -8,8 +8,8 @@
 
 console.log(
   `
-%cS5 - 91: Nullish Coalescing (nC) `,
-  "background: linear-gradient(to right, rgba(38, 2, 33, 0.906) 55%, rgba(22, 0, 98, 1) 99%); color:#FFD933; font-size: 24px; padding: 10px;"
+%cS5 - 96: Bulding a Generic Function `,
+  "background: linear-gradient(to right, rgba(38, 2, 33, 0.906) 55%, rgba(22, 0, 98, 1) 89%); color:#FFD933; font-size: 24px; padding: 10px;"
 );
 
 // Define a function to append messages to the console output div
@@ -28,17 +28,19 @@ function logToConsole(message: string, style: string) {
 
 // Default type which also a generic
 
-// Array is a generic
-const names95: Array<string> = ["ina", "jan"];
-// names95[0].split(" ");
+// // Array is a generic
+// const names96: Array<string> = ["ina", "jan"];
+// // names96[0].split(" ");
 
-const promise95: Promise<number> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(10);
-    reject(Error);
-  });
-});
+// const promise96: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//     reject(Error);
+//   });
+// });
 
-promise95.then((data) => {
-  // data.split("");
-});
+// promise96.then((data) => {
+//   // data.split("");
+// });
+
+//96 - Work here
