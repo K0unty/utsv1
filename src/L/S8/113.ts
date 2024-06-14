db32309fb85062e375ad85a76f5af2025cbd9dfb
K0unty <kountiapi@pm.me>
2024-06-14 06:04:24 +0530

    113

---
+++ src/L/S8/113.ts
@@ -0,0 +1,119 @@
+/* 
+113: Accessor & Parameter Decorators
+*/
+
+// helper function
+
+const blaConsTxt = function (text: string) {
+  console.log(`%c${text} `, "background:black;font-size:16px");
+};
+
+// ---
+
+console.log(
+  `
+  %cS8 - 113: Accessor & Parameter Decorators `,
+  "background: linear-gradient(to right, rgba(38, 2, 33, 0.906) 47%, rgba(22, 0, 103, 1) 89%); color:#FFD933; font-size: 24px; padding: 10px;font-style:italic"
+);
+
+// Actual work her
+
+// Decorators are for classes they are functions
+
+function Logger113(logString: string) {
+  blaConsTxt("Logger Factory");
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
+}
+
+// new decorator factory
+function WithTemplate113(template: string, hookID: string) {
+  return function (constructor: any) {
+    blaConsTxt("TEMPLATE FACTORY");
+    console.log("TEMPLATE FACTORY");
+    const hookEl = document.getElementById(hookID);
+    const p113 = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("p")!.textContent = p113.name;
+    }
+  };
+}
+
+// @Logger113("LOGGING - PERSON")
+@Logger113("LOGGIN 113")
+@WithTemplate113("<p> MyObject - From Decorator Factory </p>", "app113")
+class Person113 {
+  name = "Ina";
+
+  constructor() {
+    console.log("Creating person Object...");
+  }
+}
+blaConsTxt("Print person113");
+const pers113 = new Person113();
+console.log(pers113);
+
+// 113: work ---
+
+// Followign LOG functions are all decorators
+function Log113(target: any, propertyName: string | Symbol) {
+  console.log("Property Decorator");
+  console.log(target, propertyName);
+}
+function Log2113(target: any, name: string, descriptor: PropertyDescriptor) {
+  blaConsTxt("Print Log211 Decorator");
+  console.log("Accessor Decorator");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+function Log3113(
+  target: any,
+  name: string | symbol,
+  descriptor: PropertyDescriptor
+) {
+  blaConsTxt("Pritn Method Decorator");
+  console.log("Method Decorator");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+function Log4113(target: any, name: string | Symbol, position: number) {
+  blaConsTxt("Pring Parameter Decorator");
+  console.log("Parameter Decorator");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+class Product113 {
+  @Log113
+  title: string;
+  private _price: number;
+
+  @Log2113
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid - Shoudl be Positive Number");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3113
+  getPriceWithTax(@Log4113 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
+
+// 113
+const p1_113 = new Product113("Book", 20);
+const p2_113 = new Product113("Book", 30);
