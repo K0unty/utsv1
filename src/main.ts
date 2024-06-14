e39f28725ccffe5cbcb996b2cc580af27e73d880
K0unty <kountiapi@pm.me>
2024-06-14 23:27:30 +0530

    fix

--- src/main.ts
+++ src/main.ts
@@ -61,7 +61,7 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     </button>
     <button class="btn"> <a href="${
       new URL("./L/S9/p0/index.html", import.meta.url).href
-    }" target="_blank">S8</a>
+    }" target="_blank">S9P1</a>
     </button>
     <button class="btn"> Suck </button>
     
