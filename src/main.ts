f60705b7bc619de1dfe44aa5750ba8728eb63acf
K0unty <kountiapi@pm.me>
2024-06-14 23:21:23 +0530

    S9Fix

--- src/main.ts
+++ src/main.ts
@@ -59,6 +59,10 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
       new URL("./L/S8/index.html", import.meta.url).href
     }" target="_blank">S8</a>
     </button>
+    <button class="btn"> <a href="${
+      new URL("./L/S9/p0/index.html", import.meta.url).href
+    }" target="_blank">S8</a>
+    </button>
     <button class="btn"> Suck </button>
     
   <div>
