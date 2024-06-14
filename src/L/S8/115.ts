52407ce1685b7fca075598bc961c659d306ceec0
K0unty <kountiapi@pm.me>
2024-06-14 08:46:53 +0530

    add

--- src/L/S8/115.ts
+++ src/L/S8/115.ts
@@ -124,9 +124,23 @@ class Product115 {
 const p1_115 = new Product115("Book", 20);
 const p2_115 = new Product115("Book", 30);
 
+function AutoBind_115(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
 class Printer115 {
   message = "This Works!";
 
+  @AutoBind_115
   showMessage() {
     console.log(this.message);
   }
