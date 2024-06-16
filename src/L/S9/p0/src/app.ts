06c246f27bd368f79e8e560e4ce00e8a95560e00
K0unty <kountiapi@pm.me>
2024-06-16 07:40:34 +0530

    132

--- src/L/S9/p0/src/app.ts
+++ src/L/S9/p0/src/app.ts
@@ -21,14 +21,23 @@ class Project {
 
 // Project State Management - Class that manages the objects created by fillign the form
 
-type Listener = (items: Project[]) => void;
+type Listener<T> = (items: T[]) => void;
 
-class ProjectState {
-  private listeners: Listener[] = [];
+class State<T> {
+  protected listeners: Listener<T>[] = [];
+
+  addListener(listenerFn: Listener<T>) {
+    this.listeners.push(listenerFn);
+  }
+}
+
+class ProjectState extends State<Project> {
   private projects: Project[] = [];
   private static instance: ProjectState;
 
-  private constructor() {}
+  private constructor() {
+    super();
+  }
 
   static getInstance() {
     if (this.instance) {
@@ -36,10 +45,6 @@ class ProjectState {
     }
     this.instance = new ProjectState();
     return this.instance;
-  }
-
-  addListener(listenerFn: Listener) {
-    this.listeners.push(listenerFn);
   }
 
   addProject(title: string, description: string, numOfPeople: number) {
